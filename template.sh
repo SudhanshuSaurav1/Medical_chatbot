b844509a9c4d73df841689d750fd45aebc9c8121
Your Name <you@example.com>
2026-01-16 14:59:53 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,16 @@
+# creating directory
+mkdir -p src
+mkdir -p research
+
+#creating files
+touch src/_init_.py
+touch src/helper.ipynb
+touch src/prompt.ipynb
+touch .env
+touch setup.ipynb
+touch app.ipynb
+touch research/trial.ipynb
+touch requirements.txt
+
+echo 'Directory and files created successfully!.'
+
